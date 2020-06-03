1b79a53986db15dfab9a10cd3bd1fcee5c19c9cb
huabin <1351195777@qq.com>
2020-06-03 18:06:57 +0800

    初始化

---
+++ sc-f-chapter6/config-server/src/test/java/scfchapter6/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+//package scfchapter6.configserver;
+//
+//import org.junit.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ConfigServerApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
