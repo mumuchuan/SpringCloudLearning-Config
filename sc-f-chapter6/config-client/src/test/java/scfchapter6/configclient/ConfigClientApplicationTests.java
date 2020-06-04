2318ee3f9995e36d651bdc6135e8002bfbf6aae6
huabin <1351195777@qq.com>
2020-06-04 15:23:03 +0800

    配置服务

---
+++ sc-f-chapter6/config-client/src/test/java/scfchapter6/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+//package scfchapter6.configclient;
+//
+//import org.junit.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ConfigClientApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
